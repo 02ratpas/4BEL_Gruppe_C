7868868caa32b52fb579407bf00bb47f83d9aa2d
02ratpas <02ratpas@rgtfo-me.it>
2020-04-24 10:27:18 +0200

    Menue_Struktur

--- Fork_4BEL_2010/Fork_4BEL_2010/Program.cs
+++ Fork_4BEL_2010/Fork_4BEL_2010/Program.cs
@@ -3,13 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 // Umrechnung in verschiedene Zahlensysteme
+// Gruppe C
 namespace Fork_4BEL_2010
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int Zahl_1 = 0;
+            int Zahl_2 = 0;
+            int Auswahl = 0;
+            int Ergebnis = 0;
+
+
+
+            Console.WriteLine("Wähle: 1 (Binär -> Dezimal) 2 (Binär -> Hexadezimal");
+            Auswahl = Convert.ToInt16(Console.ReadLine());
+
+
+            //Schrötter, zahleneingabe
+
+
+            if (Auswahl == 1)
+                Ergebnis = Dezimal(Zahl_1, Zahl_2);
+
+            if (Auswahl == 2)
+                Ergebnis = Hexadezimal(Zahl_1, Zahl_2);
+
+
+            Console.WriteLine("Ergebnis = " + Ergebnis);
             Console.ReadKey();
+        }
+
+
+
+        static int Dezimal(int Zahl_1, int Zahl_2)
+        {
+            int Ergebnis = 0;
+
+            //Pötti, Dezimal Rechnung
+
+            return Ergebnis;
+        }
+        static int Hexadezimal(int Zahl_1, int Zahl_2)
+        {
+            int Ergebnis = 0;
+
+            //Ratti, Hexa rechnung
+
+            return Ergebnis;
         }
     }
 }
