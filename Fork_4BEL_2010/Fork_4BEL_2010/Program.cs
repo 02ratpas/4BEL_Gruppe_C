327a0409d6e90cfd08fe65dd8f4fec76124303be
ratitt <01auerap@rgtfo-me.it>
2020-04-26 20:48:59 +0200

    Hexaumwandeln hinzugefügt

--- Fork_4BEL_2010/Fork_4BEL_2010/Program.cs
+++ Fork_4BEL_2010/Fork_4BEL_2010/Program.cs
@@ -10,6 +10,8 @@ namespace Fork_4BEL_2010
     {
         static void Main(string[] args)
         {
+            string str_Zahl_1="1111";
+            string str_Ergebnis="";
             int Zahl_1 = 0;
             int Zahl_2 = 0;
             int Auswahl = 0;
@@ -28,10 +30,10 @@ namespace Fork_4BEL_2010
                 Ergebnis = Dezimal(Zahl_1, Zahl_2);
 
             if (Auswahl == 2)
-                Ergebnis = Hexadezimal(Zahl_1, Zahl_2);
+                str_Ergebnis = Hexadezimal(str_Zahl_1);
 
 
-            Console.WriteLine("Ergebnis = " + Ergebnis);
+            Console.WriteLine("Ergebnis = " + str_Ergebnis);
             Console.ReadKey();
         }
 
@@ -45,13 +47,81 @@ namespace Fork_4BEL_2010
 
             return Ergebnis;
         }
-        static int Hexadezimal(int Zahl_1, int Zahl_2)
+        static string Hexadezimal(string bi)
         {
-            int Ergebnis = 0;
-
-            //Ratti, Hexa rechnung
-
-            return Ergebnis;
+            //Raphael-Hexa-Umrechner
+            string Ergebnis="";
+            string Nibbel="";
+            for (int i = 0; i != bi.Length % 4; )
+                bi=bi.Insert(0, "0");
+            for (int i = 0; i < bi.Length; i++)
+            {
+                Nibbel += bi[i];
+                if (Nibbel.Length == 4)
+                {
+                    Ergebnis+=Nibbel_Converter(Nibbel);
+                    Nibbel = "";
+                }
+            }
+                return Ergebnis;
+        }
+        static string Nibbel_Converter(string Nibbel)
+        { 
+            string Hexaziffer="";
+            switch (Nibbel)
+            { 
+                case "0000":
+                    Hexaziffer = "0";
+                    break;
+                case "0001":
+                    Hexaziffer = "1";
+                    break;
+                case "0010":
+                    Hexaziffer = "2";
+                    break;
+                case "0011":
+                    Hexaziffer = "3";
+                    break;
+                case "0100":
+                    Hexaziffer = "4";
+                    break;
+                case "0101":
+                    Hexaziffer = "5";
+                    break;
+                case "0110":
+                    Hexaziffer = "6";
+                    break;
+                case "0111":
+                    Hexaziffer = "7";
+                    break;
+                case "1000":
+                    Hexaziffer = "8";
+                    break;
+                case "1001":
+                    Hexaziffer = "9";
+                    break;
+                case "1010":
+                    Hexaziffer = "a";
+                    break;
+                case "1011":
+                    Hexaziffer = "b";
+                    break;
+                case "1100":
+                    Hexaziffer = "c";
+                    break;
+                case "1101":
+                    Hexaziffer = "d";
+                    break;
+                case "1110":
+                    Hexaziffer = "e";
+                    break;
+                case "1111":
+                    Hexaziffer = "f";
+                    break;
+                default:
+                    break;        
+            }
+            return Hexaziffer;
         }
     }
 }
