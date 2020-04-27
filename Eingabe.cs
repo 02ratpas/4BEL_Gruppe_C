ed46d1d7ac4436cd6599c3223ac48cd5176d865d
01schmat <01schmat@rgtfo-me.it>
2020-04-27 17:13:01 +0200

    eingabe hinzugefügt

---
+++ Eingabe.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// Umrechnung in verschiedene Zahlensysteme
+// Gruppe C
+namespace Fork_4BEL_2010
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Zahl_1 = 0;
+            int Auswahl = 0;
+            int Ergebnis = 0;
+
+
+
+            Console.WriteLine("Wähle: 1 (Binär -> Dezimal) 2 (Binär -> Hexadezimal");
+            Auswahl = Convert.ToInt16(Console.ReadLine());
+
+
+            //Schrötter, Eingabe
+            Console.WriteLine("Geben Sie eine Zahle ein");
+            Zahl_1 = Convert.ToInt16(Console.ReadLine());
+
+
+            if (Auswahl == 1)
+                Ergebnis = Dezimal(Zahl_1);
+
+            if (Auswahl == 2)
+                Ergebnis = Hexadezimal(Zahl_1);
+
+
+            Console.WriteLine("Ergebnis = " + Ergebnis);
+            Console.ReadKey();
+        }
+
+
+
+        static int Dezimal(int Zahl_1)
+        {
+            int Ergebnis = 0;
+
+            //Pötti, Dezimal Rechnung
+
+            return Ergebnis;
+        }
+        static int Hexadezimal(int Zahl_1)
+        {
+            int Ergebnis = 0;
+
+            //Ratti, Hexa rechnung
+
+            return Ergebnis;
+        }
+    }
+}
